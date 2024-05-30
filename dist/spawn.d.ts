4e472c7fffd2345f802c8768701cfecedd9f2c46
Pieter Ouwerkerk <pieter.ouwerkerk@gmail.com>
2024-05-30 01:46:20 -0400

    Force args to be strings

--- dist/spawn.d.ts
+++ dist/spawn.d.ts
@@ -4,16 +4,16 @@ type spawnOptions = {
     stdio?: any;
     input?: string;
 };
-export declare function spawn(command: string, args?: any[], options?: spawnOptions): Promise<unknown> & {
+export declare function spawn(command: string, args?: string[], options?: spawnOptions): Promise<unknown> & {
     process: any;
 };
 export default spawn;
-export declare const verbose: (command: string, args?: any[], options?: spawnOptions) => Promise<unknown> & {
+export declare const verbose: (command: string, args?: string[], options?: spawnOptions) => Promise<unknown> & {
     process: any;
 };
-export declare const stderr: (command: string, args?: any[], options?: spawnOptions) => Promise<unknown> & {
+export declare const stderr: (command: string, args?: string[], options?: spawnOptions) => Promise<unknown> & {
     process: any;
 };
-export declare const silent: (command: string, args?: any[], options?: spawnOptions) => Promise<unknown> & {
+export declare const silent: (command: string, args?: string[], options?: spawnOptions) => Promise<unknown> & {
     process: any;
 };
