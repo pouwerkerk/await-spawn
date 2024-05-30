f435d1ddeca8a394fbf673a38e646af186de9efd
Pieter Ouwerkerk <pieter.ouwerkerk@gmail.com>
2024-05-30 01:47:13 -0400

    Use SpawnOptions & StdioOptions from child_process

--- dist/spawn.d.ts
+++ dist/spawn.d.ts
@@ -1,34 +1,20 @@
 /// <reference types="node" />
-type spawnOptions = {
+import { SpawnOptions } from "child_process";
+interface AwaitSpawnOptions extends SpawnOptions {
     captureStdio?: boolean;
     rejectOnExitCode?: boolean;
-    stdio?: stringOrArr;
     input?: string;
-    cwd?: string;
-    env?: object;
-    argv0?: string;
-    detached?: boolean;
-    uid?: number;
-    gid?: number;
-    serialization?: string;
-    shell?: boolean | string;
-    windowsVerbatimArguments?: boolean;
-    windowsHide?: boolean;
-    signal?: NodeJS.Signals;
-    timeout?: number;
-    killSignal?: string | number;
-};
-export declare function spawn(command: string, args?: string[], options?: spawnOptions): Promise<unknown> & {
+}
+export declare function spawn(command: string, args?: string[], options?: AwaitSpawnOptions): Promise<unknown> & {
     process: any;
 };
 export default spawn;
-export declare const verbose: (command: string, args?: string[], options?: spawnOptions) => Promise<unknown> & {
+export declare const verbose: (command: string, args?: string[], options?: AwaitSpawnOptions) => Promise<unknown> & {
     process: any;
 };
-export declare const stderr: (command: string, args?: string[], options?: spawnOptions) => Promise<unknown> & {
+export declare const stderr: (command: string, args?: string[], options?: AwaitSpawnOptions) => Promise<unknown> & {
     process: any;
 };
-export declare const silent: (command: string, args?: string[], options?: spawnOptions) => Promise<unknown> & {
+export declare const silent: (command: string, args?: string[], options?: AwaitSpawnOptions) => Promise<unknown> & {
     process: any;
 };
-type stringOrArr = string | string[];
